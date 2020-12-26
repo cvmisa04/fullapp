1283c9628da6d046eeca3924e1da0d7ee3d00194
Milos Cvetkovic <milos94partes@hotmail.com>
2020-12-27 00:36:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+    const app = express()
+    app.set('view engine','ejs')
+
+
+app.get('/',(req,res)=>{
+    res.send("OK")
+})
+
+
+
+
+app.listen('3000',()=>{
+    console.log('Slusamo na portu 3000')
+})
